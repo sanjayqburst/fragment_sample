3ccb1ea334213707b6c81335bb895e48c7312744
sanjayqburst <sanjay.mohan@qbusrt.com>
2021-11-16 12:08:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragment_sample/first_fragment.kt
@@ -0,0 +1,34 @@
+package com.example.fragment_sample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+
+
+class FirstFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view=inflater.inflate(R.layout.fragment_first_fragment, container, false)
+        val btn:Button=view.findViewById(R.id.fragmentBtn)
+        btn.setOnClickListener {
+            val fragment=SecondFragment()
+            val textData:EditText=view.findViewById(R.id.fragment1text)
+            val bundle=Bundle()
+            bundle.
+            fragment.arguments=bundle.putString("data",textData.text.toString())
+            fragmentManager?.beginTransaction()?.replace(R.id.fragmentContainer,fragment)?.commit()
+        }
+        // Inflate the layout for this fragment
+        return view
+    }
+
+}
